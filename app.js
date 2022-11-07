28a21783882767dfa3b460219b951dd8d91002ae
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-11-07 14:11:33 +0200

    ErrorHandle

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const errorHandlers = require('./handlers/errorHandler');
+const errorHandlers = require('./handlers/errorHandlers');
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if(process.env.ENV === "DEVELOPMENT"){
+    app.use(errorHandlers.developmentErrors)
+
+}else{
+    app.use(errorHandlers.productionErrors)
+}
 
 
 
