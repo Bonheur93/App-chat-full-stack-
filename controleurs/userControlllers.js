c1aff400d5716df2367dc550fd2c9419ab8777d5
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-11-08 16:33:48 +0200

    Import de Dotenv dans le server

--- controleurs/userControlllers.js
+++ controleurs/userControlllers.js
@@ -1,15 +1,17 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+exports.register = async (req, res) => {
+    const { name, email, password } = req.body;
+    const emailRejex = /[@gmail.com|@yahoo.com|@hotmail.com|@live.com]$/;
 
-exports.register = async (req, res) =>{
- const {name, email, password}= req.body;
- const emailRejex = /[@gmail.com|@yahoo.com|@hotmail.com|@live.com]$/;
+    if (!emailRejex.test(email)) throw "Email nest pas supporté depuis votre domain.";
+    if (password.length < 6) throw "Le mot de pass doit avoir contenir 6 caractères.";
 
- if (!emailRejex.test(email)) throw "Email nest pas supporté depuis votre domain.";
- if (password.length < 6) throw "Le mot de pass doit avoir contenir 6 caractères.";
-}
+    const user = new User({ name, email, password });
+    await user.save();
+};
 
-
-exports.login = async (req, res) =>{
+exports.login = async (req, res) => {
 
 }
 
--- routes/User.js
+++ routes/User.js
@@ -3,7 +3,6 @@ const {catchErrors} = require("../handlers/errorHandlers");
 const userController = require("../controleur/userController");
 
 router.post("/login", catchErrors(userController.login));
-
 router.post("/register", catchErrors(userController.register));
   
 
