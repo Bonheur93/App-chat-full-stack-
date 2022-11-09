e59e58e8d7236999b1c56fd92a4961542e1a8eb5
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-11-09 15:26:08 +0200

    EmailRejex

--- controleurs/userControlllers.js
+++ controleurs/userControlllers.js
@@ -5,7 +5,7 @@ const jwt = require('jwt-then');
 
 exports.register = async (req, res) => {
     const { name, email, password } = req.body;
-    const emailRejex = /[@gmail.com|@yahoo.com|@hotmail.com|@live.com]$/;
+    const emailRejex = /@gmail.com|@yahoo.com|@hotmail.com|@live.com]/;
 
     if (!emailRejex.test(email)) throw "Email nest pas supporté depuis votre domain.";
     if (password.length < 6) throw "Le mot de pass doit avoir contenir 6 caractères.";
