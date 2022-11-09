3b1e5b680cde9919d5a83fd3446eeb73f6852793
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-11-09 16:15:03 +0200

    authentification

--- controleurs/userControlllers.js
+++ controleurs/userControlllers.js
@@ -32,7 +32,6 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
-    console.log(req.body)
 
     const user = await User.findOne({
         email,
