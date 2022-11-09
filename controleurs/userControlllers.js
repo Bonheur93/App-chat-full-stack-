2083f95d4c52bad8374aa37738bfaf8a81c01709
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-11-09 11:42:18 +0200

    Refactorer les Controlleurs

--- controleurs/userControlllers.js
+++ controleurs/userControlllers.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+
 exports.register = async (req, res) => {
     const { name, email, password } = req.body;
     const emailRejex = /[@gmail.com|@yahoo.com|@hotmail.com|@live.com]$/;
@@ -7,11 +8,26 @@ exports.register = async (req, res) => {
     if (!emailRejex.test(email)) throw "Email nest pas supporté depuis votre domain.";
     if (password.length < 6) throw "Le mot de pass doit avoir contenir 6 caractères.";
 
-    const user = new User({ name, email, password });
+    const user = new User({ 
+        name, 
+        email, 
+        password });
+
     await user.save();
+    res.json({
+        message : "l'utilisateur [" + name + "] est enregistré avec succès",
+    });
+
 };
 
 exports.login = async (req, res) => {
+const { email, password} = req.body;
+ const user = await User.findOne({
+    email,
+    password });
+    
+if (!user) throw "Email et le mot de pass ne sont pas correct";
 
-}
+}; 
+
 
--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {catchErrors} = require("../handlers/errorHandlers");
-const userController = require("../controleur/userController");
+const userController = require("../controleurs/userControlllers");
 
 router.post("/login", catchErrors(userController.login));
 router.post("/register", catchErrors(userController.register));
