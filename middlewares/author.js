fe191961c5b6e2028d6cda6139969cc0088eb3b3
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-11-10 10:09:06 +0200

    Autorisation : Token et payload

--- middlewares/author.js
+++ middlewares/author.js
@@ -1,8 +1,14 @@
 const jwt = require("jwt-then")
 
 module.exports = async (req, res, next) => {
-    const token = req.header.authorization.split("")[1];
+    try{
+        if(req.header.authorization) throw "interdit!"
+        const token = req.header.authorization.split("")[1];
 
     const payload = await jwt.verify(token, process.env.SECRET)
     next();
-}
+
+    }catch(err){
+
+    }
+};
